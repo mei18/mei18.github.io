6aaaee72c191bd1eca501dcda66538d511e7d720
Esteban Padilla <ep@estebanpadilla.com>
2017-03-27 12:05:18 -0600

    Save changes.

--- app.js
+++ app.js
@@ -1,15 +1,50 @@
-window.addEventListener("load",init,false);
+window.addEventListener("load", init, false);
 
 function init() {
-  var pool = [];
+  console.log("init");
+  window.addEventListener("keydown", onKeyDown);
+  window.addEventListener("keyup", onKeyUp);
 
+  let pool = [];
   let hero = Hero(Vector(635, 476));
-    pool.push(hero);
 
-  function update(){
-    console.log("hi");
+  //1. Recuerda agregar todos los elementos del juego que necesitan animacion
+  //al pool de objecto.
+  pool.push(hero);
+
+  function update() {
+    //2. Recuerda que para animar los objectos hay que llamar a sus
+    // update() como se hace en el for loop
+    pool.forEach(function (gameObject) {
+      gameObject.update();
+    }, this);
 
     requestAnimationFrame(update);
   }
+
   update();
+
+  function onKeyDown(e) {
+    switch (e.keyCode) {
+      case 32:
+        hero.canJump = true;
+        break;
+      default:
+        break;
+    }
+    //3. Para que no se mueva el scroll en la pagina
+    e.preventDefault();
+  }
+
+  function onKeyUp(e) {
+    switch (e.keyCode) {
+      case 32:
+        hero.canJump = false;
+        break;
+      default:
+        break;
+    }
+    //3. Para que no se mueva el scroll en la pagina
+    e.preventDefault();
+  }
 }
--- js/hero.js
+++ js/hero.js
@@ -1,51 +1,62 @@
-
-
 function Hero(position) {
-  if (!(this instanceof Hero)) {
+
+    if (!(this instanceof Hero)) {
         return new Hero(position);
     }
-   this.character = $("#character");
-   this.height = 467;
-   this.isJumping = false;
-   this.velY = 0;
-   this.speed = 3;
-   this.y = this.height - 5;
-   this.keys = [];
-   this.gravity = 0.3;
-   this.position = position;
-   this.jump = new TweenMax(this.character, 2, {y:0});
+
+    this.image = document.getElementById("character");
+    this.isJumping = false;
+    this.canJump = false;
+    this.jumpTween = null;
+    this.timeLine = null;
+
 }
+
 Hero.prototype.update = function () {
-    //add velocity
-    this.position.add(this.speed);
-    this.render();
+    if (this.canJump) {
+        if (!this.isJumping) {
+            this.jump();
+        }
+    }
+}
+
+Hero.prototype.jump = function () {
+    //Se crean los objecto para hacer la animacion solo una ves, por eso de inician en null cuando se 
+    //construye el objecto
+    if (this.jumpTween == null && this.timeLine == null) {
+
+        this.jumpTween = new TweenLite.to(this.image, 0.5, { y: -150, ease: Back.easeIn });
+        //Se crea el time line  y se pasa el call back y el parametro porque se necesita tener la instancia
+        //de este objecto sino se pierde.
+        this.timeLine = new TimelineLite({ onComplete: completeHandler, onCompleteParams: [this], onReverseComplete: onReverseComplete, onReverseCompleteParams: [this] });
+        this.timeLine.add(this.jumpTween);
+
+        //El callbac resive la instancia del este objecto y le hace reverse al timeLine
+        function completeHandler(heroInstance) {
+            if (heroInstance) {
+                // heroInstance.isJumping = false;
+                console.log('completeHandler');
+                heroInstance.timeLine.reverse();
+            }
+        }
+        //El callbac resive la instancia del este objecto y
+        //setea la variable isJumping a false cuando terminar de brincar
+        //para poder brinca nuevamente.
+        function onReverseComplete(heroInstance) {
+            if (heroInstance) {
+                console.log('onReverseComplete');
+                heroInstance.isJumping = false;
+
+            }
+        }
+    } else {
+        //Si ya los objecto de tween fueron creados solo decirle a la animacion que reinicie.
+        this.timeLine.restart();
+    }
+
+    this.isJumping = true;
 }
 
 Hero.prototype.render = function () {
-  if (this.keys[32]) {
-      // up space
-      console.log("hello");
-    if(!this.isJumping){
-     this.jump = new TweenMax(character, 2, {y:0});
-     this.isJumping = true;
-     this.velY =- this.speed*2;
-    }
-  }
 
-  this.velY += this.gravity;
-
-  this.y += this.velY;
-
-  if(this.y >=1600 - this.height){
-      this.y = 1600 - this.height;
-      this.isJumping = false;
-  }
-
-  document.body.addEventListener("keydown", function(e) {
-      this.keys[e.keyCode] = true;
-  });
-
-  document.body.addEventListener("keyup", function(e) {
-      this.keys[e.keyCode] = false;
-  });
 }
