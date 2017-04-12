62a3db5f6b1c54c772c55a1c08e4f8fbccb47b45
Melanie Liang <melly.strearshy@gmail.com>
2017-04-12 09:05:21 -0600

    error

--- js/enemy.js
+++ js/enemy.js
@@ -12,33 +12,28 @@ function Enemy(position) {
     this.enemys[0].src = "../img/EBlue.png";
     this.enemys[1]= new Image();
     this.enemys[1].src = '../img/ERed.png';
-    this.enemys = new Image();
-    this.enemys[2].src = '../img/EBrown.png';
-    this.enemys = new Image();
-    this.enemys[3].src = '../img/EYellow.png';
-    this.enemys = new Image();
-    this.enemys[4].src = '../img/icicle.png';
-    this.enemys = new Image();
-    this.enemys[5].src = '../img/lava.png';
-    this.enemys = new Image();
-    this.enemys[6].src = '../img/stoneShard.png';
-    this.enemys = new Image();
-    this.enemys[7].src = '../img/WBlue.png';
-    this.enemys = new Image();
-    this.enemys[8].src = '../img/WBrown.png';
-    this.enemys = new Image();
-    this.enemys[9].src = '../img/WRed.png';
-    this.enemys = new Image();
-    this.enemys[10].src = '../img/WYellow.png';
 
     //console.log("array de enemigos" , this.enemys);
     //console.log('img del enemy en el html',this.image);
+    // this.enemys = new Image();
+    // this.enemys[2].src = '../img/EBrown.png';
+    // this.enemys = new Image();
+    // this.enemys[3].src = '../img/EYellow.png';
+    // this.enemys = new Image();
+    // this.enemys[4].src = '../img/icicle.png';
+    // this.enemys = new Image();
+    // this.enemys[5].src = '../img/lava.png';
+    // this.enemys = new Image();
+    // this.enemys[6].src = '../img/stoneShard.png';
+    // this.enemys = new Image();
+    // this.enemys[7].src = '../img/WBlue.png';
+    // this.enemys = new Image();
+    // this.enemys[8].src = '../img/WBrown.png';
+    // this.enemys = new Image();
+    // this.enemys[9].src = '../img/WRed.png';
+    // this.enemys = new Image();
+    // this.enemys[10].src = '../img/WYellow.png';
 
-    for(var i = 0; i < this.enemys.length; i++) {
-      //sacar la img en random.
-      var randomimg = this.enemys[Math.floor(Math.random() * this.enemys.length)];
-      //console.log('random',randomimg.src);
-    }
 }
 
 Enemy.prototype.update = function () {
@@ -52,24 +47,29 @@ Enemy.prototype.update = function () {
 Enemy.prototype.render = function () {
   //esto es solo para debug para ver si esta moviendo.
   this.image.setAttribute('transform', 'translate(' + this.position.x + ',' + this.position.y + ')');
-
+  for(var i = 0; i < this.enemys.length; i++) {
+    //sacar la img en random.
+    var randomimg = this.enemys[Math.floor(Math.random() * this.enemys.length)];
+    //console.log('random',randomimg.src);
+  }
   //Display.
   var img = document.getElementById("img-enemy");
 
-  for(var i = 0; i < this.enemys.length;i++){
-    if(this.enemys[i].src == img.src){
-      console.log('dentro del for');
+    for(var i = 0; i < this.enemys.length;i++){
+        if(this.enemys[i].src == img.src)
+        {
+          console.log('dentro del for');
 
-      if(i === this.enemys.length){
-        img.setAttribute("src"," this.enemys[0].src");
-        console.log('i es igual!!');
-      }
-      console.log('i no es igual!!');
+            if(i === this.enemys.length){
+                img.setAttribute("src"," this.enemys[0].src");
+                console.log('i es igual!!');
+            }
+            console.log('i no es igual!!');
 
-      //document.getElementById(element).src = this.enemys[i+1].src;
-      //break;
+            //document.getElementById(element).src = this.enemys[i+1].src;
+            //break;
+        }
     }
-  }
 }
 
 Enemy.prototype.checkBounderies = function () {
