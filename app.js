6e5907dc1459c09278836e8577f347b91e3da52d
Melanie Liang <melly.strearshy@gmail.com>
2017-04-23 10:06:37 -0600

    hero

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ function init() {
   window.addEventListener("keydown", onKeyDown);
   window.addEventListener("keyup", onKeyUp);
 
-  let hero = Hero(Vector(width / 2, height / 2.9));
+  let hero = Hero(Vector(width / 2, height / 2.8));
   console.log("heroe", hero);
   $('#character').css('background', '#2C81B7');
 
