321584429826455cb49d9470c9e85b28e1fbd418
Melanie Liang <melly.strearshy@gmail.com>
2017-04-23 10:00:49 -0600

    hero

--- js/hero.js
+++ js/hero.js
@@ -5,10 +5,11 @@ function Hero(position) {
     }
 
     this.position = position;
-    this.width = 120;
+    this.width = 100;
     this.height = 100;
 
     this.origin = Vector(position.x, position.y);
+    console.log("origin", this.origin);
     this.image = document.getElementById("character");
     this.canJump = false;
 
